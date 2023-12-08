1b22167680fd9b46befb2a22af6289619303af03
rk-prasanth1 <prasanth.rk44@gmail.com>
2023-12-08 12:34:24 +0530

    remove copy code

--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "eu-north-1"
-}
-
-resource "aws_instance" "terraform" {
-  ami = var.ami_value
-  instance_type = var.instance_type_value
-}
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-ami_value ="ami-0fe8bec493a81c7da"
-instance_type_value = "t3.micro"
